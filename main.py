a9893cb39b1db55de2ef139d17bb8d6861b3bf49
Lukas Panneke <l.panneke@googlemail.com>
2022-07-05 20:54:28 +0200

    GPIO 3 gibt nur noch 1 aus, also benutzen wir statdessen GPIO 16

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     pi = pigpio.pi()
     motor1: StepperMotor = StepperMotor(sleep_pin=10, dir_pin=20, step_pin=21, mode_pins=(14, 15, 18),
                                         resolution=RESOLUTION.QUARTER, pi=pi)
-    motor2: StepperMotor = StepperMotor(sleep_pin=9, dir_pin=23, step_pin=24, mode_pins=(2, 3, 4),
+    motor2: StepperMotor = StepperMotor(sleep_pin=9, dir_pin=23, step_pin=24, mode_pins=(2, 16, 4),
                                         resolution=RESOLUTION.QUARTER, pi=pi)
     motor3: StepperMotor = StepperMotor(sleep_pin=11, dir_pin=19, step_pin=26, mode_pins=(5, 6, 13),
                                         resolution=RESOLUTION.QUARTER, pi=pi)
